c3d5238b981400039dab58c2a935d7ac11d0f2bb
CosmoCarpincho <cosmocarpincho@gmail.com>
2023-11-20 20:19:18 -0300

    ej 1.22 con buffers

--- cap_01/ej/ej_1_22.c
+++ cap_01/ej/ej_1_22.c
@@ -24,7 +24,7 @@ int load_buffer(char buff[], int init, int n_total_char);
 int load_other_buffer(char buff[], char buff2[], int n_total_char,
                       int n_final_char);
 
-// Retorna 0 .. n  númeors de caracteres que hay pasar al otro buffer
+// Retorna 0 .. n  números de caracteres que hay pasar al otro buffer
 // Retorna EOF cuando ya no hay mas datos de stdin
 int load_buffer(char buff[], int init, int n_total_char) {
 
@@ -34,13 +34,8 @@ int load_buffer(char buff[], int init, int n_total_char) {
     buff[i] = c;
     if (c != ' ' && c != '\t' && c != '\n')
       ++n_final_char;
-    else {
+    else
       n_final_char = 0;
-      if (i == N_TOTAL_CHAR - 1) {
-        buff[i - 1] = '\n'; // ver esto
-        break;              // ver esto
-      }
-    }
   }
   buff[i] = '\0';
 
@@ -54,12 +49,12 @@ int load_buffer(char buff[], int init, int n_total_char) {
 int load_other_buffer(char buff[], char buff2[], int n_total_char,
                       int n_final_char) {
   int i;
+  int i_first_final_char = n_total_char - n_final_char;
   for (i = 0; i < n_final_char; ++i) {
-    buff2[i] = buff[i + n_total_char];
+    buff2[i] = buff[i + i_first_final_char];
   }
-
-  buff[N_TOTAL_CHAR - n_final_char] = '\n';     // ver esto
-  buff[N_TOTAL_CHAR - n_final_char + 1] = '\0'; // ver esto
+  buff2[i] = '\0';
+  buff[i_first_final_char] = '\0';
 
   return i;
 }
@@ -67,25 +62,39 @@ int load_other_buffer(char buff[], char buff2[], int n_total_char,
 int main() {
   //
   int state = ON_BUFF_1;
-  char buff[N_TOTAL_CHAR];
-  char buff2[N_TOTAL_CHAR];
+  char buff[N_TOTAL_CHAR + 1];
+  char buff2[N_TOTAL_CHAR + 1];
+  char *p_buffer;
+  char *p_other_buffer;
   int n_final_char = 0;
   int init = 0;
 
   do {
 
+    // swap buffers
     if (state == ON_BUFF_1) {
-      n_final_char = load_buffer(buff, n_final_char, N_TOTAL_CHAR);
-      init = load_other_buffer(buff, buff2, N_TOTAL_CHAR, n_final_char);
-      buff[N_TOTAL_CHAR - n_final_char] = '\n';
-      printf("%s", buff);
+      p_buffer = buff;
+      p_other_buffer = buff2;
       state = ON_BUFF_2;
-
     } else if (state == ON_BUFF_2) {
-      n_final_char = load_buffer(buff2, n_final_char, N_TOTAL_CHAR);
-      init = load_other_buffer(buff2, buff, N_TOTAL_CHAR, n_final_char);
-      printf("%s", buff2);
+      p_buffer = buff2;
+      p_other_buffer = buff;
       state = ON_BUFF_1;
+    }
+
+    n_final_char = load_buffer(p_buffer, init, N_TOTAL_CHAR);
+
+    if (n_final_char > 0 && n_final_char != N_TOTAL_CHAR) {
+      init = load_other_buffer(p_buffer, p_other_buffer, N_TOTAL_CHAR,
+                               n_final_char);
+    } else {
+      init = 0;
+      if (n_final_char != EOF)
+        n_final_char = 0;
+    }
+    printf("%s", p_buffer);
+    if (p_buffer[N_TOTAL_CHAR - n_final_char - 1] != '\n') {
+      printf("\n");
     }
 
   } while (n_final_char != EOF);
