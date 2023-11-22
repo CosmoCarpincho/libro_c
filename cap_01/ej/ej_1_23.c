7baed425b19bdb9cbc76b47e5cb78ecc87824a97
CosmoCarpincho <cosmocarpincho@gmail.com>
2023-11-22 10:45:15 -0300

    ej 1.23

--- cap_01/ej/ej_1_23.c
+++ cap_01/ej/ej_1_23.c
@@ -1,4 +1,4 @@
-// Cosmo: Terminar
+// Funciona pero tiene algunos artefactos.
 /*Ejercicio 1-23. Escriba un programa para eliminar todos los comentarios de un
  * programa en C. No olvide manejar apropiadamente las cadenas entre comillas y
  * las constantes de carácter. Los comentarios de C no se anidan.*/
@@ -17,18 +17,23 @@
 // q8  1  0  6  7  0  0
 // F = {q5, q8}
 
+// Este automata tiene 1 error. Que es cuando aparece el caracter \' .
+// Me deja impar los caracteres y cuando leo con getchar me toma 2 caracteres
+// \ y '
+//  Entonces es un caso particular.
+
 #include <ctype.h>
 #include <malloc.h>
 #include <stdio.h>
 
-#define BUFF_SIZE_INIT 100
+#define BUFF_SIZE_INIT 10000
 #define IS_ANOTHER_SYMBOL -1 // cuando es otro caracter
 
 int states[][6] = {{1, 0, 6, 7, 0, 0}, {2, 3, 6, 7, 0, 0}, {2, 2, 2, 2, 8, 2},
-                   {3, 4, 3, 3, 3, 3}, {5, 4, 3, 3, 3, 3}, {1, 0, 6, 7, 0, 0},
+                   {3, 4, 3, 3, 3, 3}, {5, 4, 3, 3, 3, 3}, {1, 0, 6, 7, 8, 0},
                    {6, 6, 0, 6, 6, 6}, {7, 7, 7, 0, 7, 7}, {1, 0, 6, 7, 0, 0}};
 
-char simbolos[] = {'/', '*', '"', '\'', '\n'};
+char simbolos[] = {'/', '*', '"', '\'', '\n'}; //'
 int cant_simbolos = 5;
 
 int posicion_alfabeto(char c) {
@@ -41,6 +46,8 @@ int posicion_alfabeto(char c) {
 }
 
 int main() {
+  char *p1, *p2;
+  char aux;
   int count = 0;
   int buffer_size = BUFF_SIZE_INIT;
   char *buffer = (char *)malloc(buffer_size * sizeof(char));
@@ -76,15 +83,42 @@ int main() {
     if ((pos = posicion_alfabeto(buffer[i])) == IS_ANOTHER_SYMBOL)
       pos = 5;
 
+    /* // debbug
+        if (buffer[i] == 'X') {
+          p1 = buffer + i;
+          p2 = buffer_final + j;
+        }
+        if (i == 2624)
+          printf(" ");
+    */
     state = states[state][pos];
+
+    aux = buffer[i - 2];
+    // Pasan cosas raras en el programa
+    // use aux para que pueda tomar ese caracter. Raro porque queria ponerlo
+    // dentro de la misma linea Pero asi no funcionaba. Debe de tener que ver
+    // con el arreglo (buffers).
+
+    // Casos particulares.
+    if (state == 7 && buffer[i] == '\'' && buffer[i - 1] == '\\') { //'
+      state == 7;
+
+      if (aux == '\\')
+        state == 0;
+    }
+
     buffer_final[j++] = buffer[i];
+
     // Se borra desde q1 -> q8, q1 -> q5 y ver que pasa con q1 -> q2 si no hay
     // mas caracteres y no hay \n al final
-    if (state == 1 && comienza_guardar == 0)
-      i_primero_borrar = i;
+    if (state == 1 && comienza_guardar == 0) {
+      i_primero_borrar = --j;
+      comienza_guardar = 1;
+    }
 
     if (state == 5 || state == 8) {
       j = i_primero_borrar;
+      comienza_guardar = 0;
     }
   }
   buffer_final[j] = '\0';
